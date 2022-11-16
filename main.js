284f7f053f82a7065ff07cca1f09ec1b30c6e071
Seth Bashford <seth.bashford@boystown.org>
2022-11-16 14:23:22 -0600

    making progress

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 function audioBufferSource(jsPsych, url) {
   const audioContext = jsPsych.pluginAPI.audioContext();
   return jsPsych.pluginAPI.getAudioBuffer(url).then((buffer) => {
@@ -26,6 +25,93 @@ function moveToColumn(image, column) {
   }
 }
 
+function pixelsString(a) {
+  return `${a}px`;
+}
+
+function percentString(a) {
+  return `${a}%`;
+}
+
+function circleElementWithColorAndDiameter(color, diameterPixels) {
+  const circle = document.createElement("div");
+  circle.style.height = pixelsString(diameterPixels);
+  circle.style.width = pixelsString(diameterPixels);
+  circle.style.borderRadius = pixelsString(diameterPixels / 2);
+  circle.style.backgroundColor = color;
+  return circle;
+}
+
+class FormExampleJsPsychPlugin {
+  constructor(jsPsych) {
+    this.jsPsych = jsPsych;
+  }
+  trial(display_element, trial) {
+    const leftCircle = circleElementWithColorAndDiameter("black", 200);
+    leftCircle.style.position = "fixed";
+    leftCircle.style.left = percentString(25);
+    leftCircle.style.top = percentString(50);
+    leftCircle.style.transform = "translate(-50%, -50%)";
+    leftCircle.style.visibility = "hidden";
+    display_element.append(leftCircle);
+    const middleCircle = circleElementWithColorAndDiameter("black", 200);
+    middleCircle.style.position = "fixed";
+    middleCircle.style.left = percentString(50);
+    middleCircle.style.top = percentString(50);
+    middleCircle.style.transform = "translate(-50%, -50%)";
+    middleCircle.style.visibility = "hidden";
+    display_element.append(middleCircle);
+    const rightCircle = circleElementWithColorAndDiameter("black", 200);
+    rightCircle.style.position = "fixed";
+    rightCircle.style.right = percentString(25);
+    rightCircle.style.top = percentString(50);
+    rightCircle.style.transform = "translate(50%, -50%)";
+    rightCircle.style.visibility = "hidden";
+    display_element.append(rightCircle);
+    audioBufferSource(this.jsPsych, trial.audioUrl).then((audioSource) => {
+      audioSource.start();
+      this.jsPsych.pluginAPI.setTimeout(() => {
+        leftCircle.style.visibility = "visible";
+      }, trial.leftCircleAppearsAfterMilliseconds);
+      this.jsPsych.pluginAPI.setTimeout(() => {
+        middleCircle.style.visibility = "visible";
+      }, trial.middleCircleAppearsAfterMilliseconds);
+      this.jsPsych.pluginAPI.setTimeout(() => {
+        rightCircle.style.visibility = "visible";
+      }, trial.rightCircleAppearsAfterMilliseconds);
+      audioSource.onended = () => {
+        leftCircle.addEventListener("click", () => {
+          this.jsPsych.finishTrial();
+        });
+        middleCircle.addEventListener("click", () => {
+          this.jsPsych.finishTrial();
+        });
+        rightCircle.addEventListener("click", () => {
+          this.jsPsych.finishTrial();
+        });
+      };
+    });
+  }
+}
+
+FormExampleJsPsychPlugin.info = {
+  name: "Form Example",
+  parameters: {
+    audioUrl: {
+      type: jsPsychModule.ParameterType.AUDIO,
+    },
+    leftCircleAppearsAfterMilliseconds: {
+      type: jsPsychModule.ParameterType.INT,
+    },
+    middleCircleAppearsAfterMilliseconds: {
+      type: jsPsychModule.ParameterType.INT,
+    },
+    rightCircleAppearsAfterMilliseconds: {
+      type: jsPsychModule.ParameterType.INT,
+    },
+  },
+};
+
 class OstensiveNamingJsPsychPlugin {
   constructor(jsPsych) {
     this.jsPsych = jsPsych;
@@ -36,7 +122,7 @@ class OstensiveNamingJsPsychPlugin {
     image.style.top = "50%";
     image.style.maxWidth = "50%";
     image.style.maxHeight = "100%";
-    moveToColumn(image, trial.startingColumn)
+    moveToColumn(image, trial.startingColumn);
     image.style.visibility = "hidden";
     image.src = trial.imageUrl;
 
@@ -59,7 +145,7 @@ class OstensiveNamingJsPsychPlugin {
 }
 
 OstensiveNamingJsPsychPlugin.info = {
-  name: "PLUGIN-NAME",
+  name: "Ostensive Naming",
   parameters: {
     imageUrl: {
       type: jsPsychModule.ParameterType.IMAGE,
@@ -68,8 +154,8 @@ OstensiveNamingJsPsychPlugin.info = {
       type: jsPsychModule.ParameterType.AUDIO,
     },
     startingColumn: {
-      type: jsPsychModule.ParameterType.INT
-    }
+      type: jsPsychModule.ParameterType.INT,
+    },
   },
 };
 
@@ -115,10 +201,13 @@ function uploadToRedcap(token) {
     });
 }
 
+const ONText = "ON";
+const FormText = "Form";
+
 function startExperiment(page, conditionSelect, jsPsych) {
   document.body.removeChild(page);
   const condition = conditionSelect.options.item(
-    conditionSelect.selectedIndex,
+    conditionSelect.selectedIndex
   ).textContent;
 
   const timeline = [];
@@ -131,54 +220,73 @@ function startExperiment(page, conditionSelect, jsPsych) {
 
   timeline.push({
     type: jsPsychHtmlButtonResponse,
-    stimulus:
-      "<p style='font-size:50px;'>Click the star to begin!</p>",
+    stimulus: "<p style='font-size:50px;'>Click the star to begin!</p>",
     button_html: '<img src="stimuli/images/star.bmp"></img>',
     choices: [""],
   });
+  switch (condition) {
+    case ONText:
+      timeline.push({
+        type: jsPsychAudioKeyboardResponse,
+        trial_ends_after_audio: true,
+        choices: "NO_KEYS",
+        stimulus: `${audioDirectory}ON_intro.wav`,
+      });
 
-  timeline.push({
-    type: jsPsychAudioKeyboardResponse,
-    trial_ends_after_audio: true,
-    choices: "NO_KEYS",
-    stimulus: `${audioDirectory}ON_intro.wav`
-  });
+      timeline.push({
+        type: jsPsychHtmlButtonResponse,
+        stimulus: "",
+        button_html: '<img src="stimuli/images/star.bmp"></img>',
+        choices: [""],
+      });
 
-  timeline.push({
-    type: jsPsychHtmlButtonResponse,
-    stimulus: "",
-    button_html: '<img src="stimuli/images/star.bmp"></img>',
-    choices: [""],
-  });
+      timeline.push({
+        type: OstensiveNamingJsPsychPlugin,
+        imageUrl: `${imageDirectory}bee.bmp`,
+        audioUrl: `${audioDirectory}bee_h_sp1.wav`,
+        startingColumn: 1,
+      });
 
-  timeline.push({
-    type: OstensiveNamingJsPsychPlugin,
-    imageUrl: `${imageDirectory}bee.bmp`,
-    audioUrl: `${audioDirectory}bee_h_sp1.wav`,
-    startingColumn: 1
-  });
+      timeline.push({
+        type: jsPsychAudioKeyboardResponse,
+        trial_ends_after_audio: true,
+        choices: "NO_KEYS",
+        stimulus: `${audioDirectory}click_the_star_to_begin.wav`,
+      });
 
-  timeline.push({
-    type: jsPsychAudioKeyboardResponse,
-    trial_ends_after_audio: true,
-    choices: "NO_KEYS",
-    stimulus: `${audioDirectory}click_the_star_to_begin.wav`
-  });
+      timeline.push({
+        type: jsPsychHtmlButtonResponse,
+        stimulus: "",
+        button_html: '<img src="stimuli/images/star.bmp"></img>',
+        choices: [""],
+      });
 
-  timeline.push({
-    type: jsPsychHtmlButtonResponse,
-    stimulus: "",
-    button_html: '<img src="stimuli/images/star.bmp"></img>',
-    choices: [""],
-  });
+      timeline.push({
+        type: OstensiveNamingJsPsychPlugin,
+        imageUrl: `${imageDirectory}joast.bmp`,
+        audioUrl: `${audioDirectory}joast_t_sp1.wav`,
+        startingColumn: 2,
+      });
+      break;
+    case FormText:
+      timeline.push({
+        type: jsPsychAudioKeyboardResponse,
+        trial_ends_after_audio: true,
+        choices: "NO_KEYS",
+        stimulus: `${audioDirectory}3AFCform_intro_day1.wav`,
+      });
 
-  timeline.push({
-    type: OstensiveNamingJsPsychPlugin,
-    imageUrl: `${imageDirectory}joast.bmp`,
-    audioUrl: `${audioDirectory}joast_t_sp1.wav`,
-    startingColumn: 2
-  });
-
+      timeline.push({
+        type: FormExampleJsPsychPlugin,
+        audioUrl: `${audioDirectory}3AFCform_example1.wav`,
+        leftCircleAppearsAfterMilliseconds: 1000,
+        middleCircleAppearsAfterMilliseconds: 2000,
+        rightCircleAppearsAfterMilliseconds: 3000,
+      });
+      break;
+    default:
+      break;
+  }
   jsPsych.run(timeline);
 }
 
@@ -199,14 +307,12 @@ function main() {
   const page = createChildElement(document.body, "div");
   const conditionLabel = createChildElement(
     createChildElement(page, "div"),
-    "label",
+    "label"
   );
   conditionLabel.textContent = "Condition";
   const conditionSelect = createChildElement(conditionLabel, "select");
-  createChildElement(conditionSelect, "option").textContent =
-    "ON";
-  createChildElement(conditionSelect, "option").textContent =
-    "Form";
+  createChildElement(conditionSelect, "option").textContent = ONText;
+  createChildElement(conditionSelect, "option").textContent = FormText;
   const confirmButton = createChildElement(page, "button");
   confirmButton.textContent = "Confirm";
   confirmButton.addEventListener("click", () => {
